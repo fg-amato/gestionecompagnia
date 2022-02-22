836bf71f2291f6c0379b016fece79ef0106715b3
flavio.g.amato <flavio.g.amato@gmail.com>
2022-02-22 17:30:24 +0100

    Schema aggiornato nell'interface Constants

--- src/it/gestionecompagnia/dao/Constants.java
+++ src/it/gestionecompagnia/dao/Constants.java
@@ -3,7 +3,7 @@ package it.gestionecompagnia.dao;
 public interface Constants {
 
 	public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-	public static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/testuser?user=root&password=021299&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
+	public static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/gestionecompagnia?user=root&password=021299&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
 	// public static final String CONNECTION_URL =
 	// "jdbc:mysql://localhost:3306/testuser?user=root&password=root";
 
